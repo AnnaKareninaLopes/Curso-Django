82db3ade781de8d701b841f97e5f0022deca0ae9
Anna Karenina Lopes Silva <akareninals@gmail.com>
2024-09-17 18:49:36 -0300

    Remoções

--- recipes/views.py
+++ recipes/views.py
@@ -4,9 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
